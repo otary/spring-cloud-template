d69e0c5d7e28890e71e6f77f650139f11737ec0d
chenzw <656469722@qq.com>
2019-10-05 15:30:26 +0800

    initialization

--- assembly/bin/startup.cmd
+++ assembly/bin/startup.cmd
@@ -3,3 +3,4 @@
 set PROJECT_HOME=%~dp0..\..
 
 start cmd /k "cd %PROJECT_HOME%\spring-cloud-template-center & mvn spring-boot:run"
+start cmd /k "cd %PROJECT_HOME%\spring-cloud-template-config & mvn spring-boot:run"
